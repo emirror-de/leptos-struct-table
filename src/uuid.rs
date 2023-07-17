d49f26487c2d2313c0dfd783b7cc7cc7d4876402
Lewin Probst <info@emirror.de>
2023-07-18 00:07:37 +0200

    feat: Uuid is now implementing std::fmt::Display

--- src/uuid.rs
+++ src/uuid.rs
@@ -42,3 +42,9 @@ impl IntoView for Uuid {
         .into()
     }
 }
+
+impl std::fmt::Display for Uuid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
